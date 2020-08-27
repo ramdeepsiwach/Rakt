3485e08abcf1e7a9bfb84dfdf17b64645fe4bcdb
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-08-27 23:14:34 +0530

    :sparkles: Added Phone Verification Firebase Integration

--- app/src/main/java/com/rakt/PhoneAuth1.java
+++ app/src/main/java/com/rakt/PhoneAuth1.java
@@ -6,20 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class PhoneAuth1 extends AppCompatActivity {
 
-    Button verify;
+    private EditText phoneEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth1);
-        verify=findViewById(R.id.verify);
+
+        phoneEditText = findViewById(R.id.phoneEditText);
+
+        findViewById(R.id.requestVerification).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phoneNumber = phoneEditText.getText().toString().trim();
+
+                if(phoneNumber.isEmpty() || phoneNumber.length() < 10){
+                    phoneEditText.setError("Enter a valid Phone Number");
+                    phoneEditText.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(PhoneAuth1.this, PhoneAuth2.class);
+                intent.putExtra("phoneNumber", phoneNumber);
+                startActivity(intent);
+            }
+        });
 
     }
 
-    public void openPhoneAuth2(View view) {
-        Intent i=new Intent(this,PhoneAuth2.class);
-        startActivity(i);
-    }
 }
--- app/src/main/java/com/rakt/PhoneAuth2.java
+++ app/src/main/java/com/rakt/PhoneAuth2.java
@@ -1,20 +1,155 @@
 package com.rakt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class PhoneAuth2 extends AppCompatActivity {
+
+    private String mVerificationId;
+    private EditText otpEditText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth2);
+
+        mAuth = FirebaseAuth.getInstance();
+        otpEditText = findViewById(R.id.otpEditText);
+
+        Intent intent = getIntent();
+        String phoneNumber = intent.getStringExtra("phoneNumber");
+
+        ((EditText)findViewById(R.id.phoneEditText)).setText(phoneNumber);
+        sendVerificationCode(phoneNumber);
+
+        findViewById(R.id.verify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = otpEditText.getText().toString().trim();
+                if (code.isEmpty() || code.length() < 6) {
+                    otpEditText.setError("Enter valid code");
+                    otpEditText.requestFocus();
+                    return;
+                }
+
+                //verifying the code entered manually
+                verifyVerificationCode(code);
+            }
+        });
+
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(PhoneAuth2.this,PhoneAuth1.class);
+                startActivity(i);
+            }
+        });
     }
-    public void openPhoneAuth1FromPhoneAuth2(View view) {
-        Intent i=new Intent(this,PhoneAuth1.class);
-        startActivity(i);
+
+    //the method is sending verification code
+    //the country id is concatenated
+    //you can take the country id as user input as well
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + phoneNumber,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    }
+
+    //the callback to detect the verification status
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            //Getting the code sent by SMS
+            String code = phoneAuthCredential.getSmsCode();
+
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+            if (code != null) {
+                otpEditText.setText(code);
+                //verifying the code
+                verifyVerificationCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(PhoneAuth2.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            //storing the verification id that is sent to the user
+            mVerificationId = s;
+        }
+    };
+
+
+    private void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(PhoneAuth2.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //verification successful we will start the profile activity
+                            Intent intent = new Intent(PhoneAuth2.this, MainActivity.class);    //Todo : Update Class name
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+                        } else {
+
+                            //verification unsuccessful.. display an error message
+
+                            String message = "Something is wrong, we will fix it soon...";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Invalid code entered...";
+                            }
+
+                            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), message, Snackbar.LENGTH_LONG);
+                            snackbar.setAction("Dismiss", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                }
+                            });
+                            snackbar.show();
+                        }
+                    }
+                });
     }
 }
