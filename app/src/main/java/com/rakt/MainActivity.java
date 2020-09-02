f84746d3d075e9520037c7b9bc05dd7c0b524252
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-09-02 12:13:56 +0530

    :pencil2: Added User Persistence Check to Authentication

--- app/src/main/java/com/rakt/MainActivity.java
+++ app/src/main/java/com/rakt/MainActivity.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            // startActivity(new Intent(this, Home.class)); Todo: Update class name as per requirement
+            finish();
+        }
         setContentView(R.layout.activity_main);
     }
 
     public void openPhoneAuth1(View view) {
-        Intent intent=new Intent(this,PhoneAuth1.class);
-        startActivity(intent);
+        startActivity(new Intent(this,PhoneAuth1.class));
     }
 }
