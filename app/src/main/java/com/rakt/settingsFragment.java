b824c7ac733b21cd36f9e74398da878fe37c0073
pagalprogrammer <pagalprogrammer@protonmail.com>
2020-09-03 09:50:28 +0530

    :pencil2: Added SignOut Function

--- app/src/main/java/com/rakt/settingsFragment.java
+++ app/src/main/java/com/rakt/settingsFragment.java
@@ -1,15 +1,18 @@
 package com.rakt;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class settingsFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class settingsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,4 +20,18 @@ public class settingsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.logOutButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(),MainActivity.class));
+            }
+        });
+    }
+
 }
